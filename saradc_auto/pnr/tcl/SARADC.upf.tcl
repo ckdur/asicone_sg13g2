bc6ed5cf70b8cf408aea7402f90af2b359002142
Ckristian Duran <ckdur.iso@gmail.com>
2025-10-03 10:06:24 +0900

    Moving the SARADC to a different repository

--- saradc_auto/pnr/tcl/SARADC.upf.tcl
+++
@@ -1,3 +0,0 @@
-# create_power_domain NOTHING -elements {.}
-create_power_domain ANALOG -elements {analog analog/*}
-create_power_domain CORE -elements {. digital}
--- saradc_auto/scripts/gds2lef.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-GDS=$1 CSV=$2 magic -dnull -noconsole -rcfile $3 $SCRIPT_DIR/gds2lef.magic.tcl
-
